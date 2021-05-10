22a8620865052e79dd2eb8e95f7b5d51cd40acdf
dilyatester <Dilya.shafigullina@norbit.ru>
2021-05-10 22:55:20 +0300

    Реализованы тесты для добавления и удаления групп

--- addressbook_tests_autoit/tests/GroupRemovalTests.cs
+++ addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -34,7 +34,7 @@ namespace addressbook_tests_autoit.tests
             oldGroups.Sort();
             newGroups.Sort();
 
-            Assert.AreEqual(oldGroups, newGroups);
+            Assert.AreEqual(oldGroups, newGroups); 
         }
     }
 }
